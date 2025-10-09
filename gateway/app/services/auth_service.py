8584a918a37252c40f0a43d716d21f80eea11bf9
Kascabrel <107616396+Kascabrel@users.noreply.github.com>
2025-10-09 11:07:13 +0200

    feature: Gateway Update: proxy_auth_request Add: : It returns both the response and status code

--- gateway/app/services/auth_service.py
+++ gateway/app/services/auth_service.py
@@ -1,8 +1,11 @@
 import httpx
 from app.core.config import settings
 
-async def proxy_to_auth(path: str, method: str = "GET", data=None, headers=None):
-    url = f"{settings.AUTH_SERVICE_URL}/auth{path}"
+async def proxy_to_auth(path: str, method: str, data, headers: dict):
+    url = f"{settings.AUTH_SERVICE_URL.rstrip('/')}/{path.lstrip('/')}"
     async with httpx.AsyncClient() as client:
-        response = await client.request(method, url, json=data, headers=headers)
+        if data:
+            response = await client.request(method, url, content=data, headers=headers)
+        else:
+            response = await client.request(method, url, headers=headers)
     return response
